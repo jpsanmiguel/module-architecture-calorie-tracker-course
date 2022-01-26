7c4f64b2518e199927617638b04525aea8339231
Juan <you@example.com>
2022-01-25 20:32:29 -0500

    Migrated all files to Gradle Kotlin DSL and added files for buildSrc

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onBoardingDomain = ":onboarding:onboarding_domain"
+    const val onBoardingPresentation = ":onboarding:onboarding_presentation"
+
+    const val trackerData = ":tracker:tracker_data"
+    const val trackerDomain = ":tracker:tracker_domain"
+    const val trackerPresentation = ":tracker:tracker_presentation"
+}
